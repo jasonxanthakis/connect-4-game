7e2ceb28ba6b413ba27ba21c0db8794ae2158f05
jasonxanthakis <iasonas.xanthakis@gmail.com>
2025-07-11 12:19:33 +0100

    Successfully collected inputs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-//import { createInterface } from 'node:readline/promises';
-const { createInterface } = require('node:readline/promises');
+import { type } from 'node:os';
+import { createInterface } from 'node:readline/promises';
+//const { createInterface } = require('node:readline/promises');
 
 const rl = createInterface({
   input: process.stdin,
@@ -28,6 +29,39 @@ class Game {
 
 }
 
+class Player {
+    constructor() {
+        this.value;
+        this.numOfWins = 0;
+    };
+}
+
+class HumanPlayer extends Player {
+  getValid() {
+    const arr = [];
+    
+    for (let i = 0; i < 7; i++) {
+      arr.push('col' + i.toString());
+      arr.push('col '+ i.toString());
+      arr.push(i.toString());
+    };
+    
+    return arr;
+  };
+
+  async getValue() {
+    this.value = await getInput('Select a column (1-8): ', this.getValid(), rl);
+    this.value = this.value.charAt(this.value.length - 1);
+    this.value = Number(this.value);
+  };
+}
+
+class ComputerPlayer extends Player {
+    randomise() {
+        this.value = Math.floor(Math.random() * 7);
+    };
+}
+
 function generateGrid() {
   let array = [];
   for(let i=0; i < 6; i++){
@@ -40,9 +74,15 @@ function generateGrid() {
   return array;
 }
 
+/*
 module.exports = {
   getInput,
   outputSum
 };
+*/
 
-//process.exit(0);
+const temp = new HumanPlayer();
+await temp.getValue();
+console.log(temp.value, typeof temp.value);
+
+process.exit(0);
